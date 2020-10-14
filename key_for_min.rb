490f0b46f70ed5ca05d8c4d2c79b876e2eb18fc8
Melissa <thecodeplanner@gmail.com>
2020-10-14 18:37:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -13,13 +13,13 @@
 # end
 # end
 
-def key_for_min_value(name_hash)
-  name_hash.each do |name, num|
-    min = name_hash[num][0]   
-    if #{num} < min
-      min = #{num}
-    if #{name} == {}
-      nil 
-  end
-  #{name} 
-end
+# def key_for_min_value(name_hash)
+#   name_hash.each do |name, num|
+#     min = name_hash[num][0]   
+#     if #{num} < min
+#       min = #{num}
+#     if #{name} == {}
+#       nil 
+#   end
+#   #{name} 
+# end
